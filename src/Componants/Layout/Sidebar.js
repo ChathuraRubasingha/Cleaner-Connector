790fd4dbe73f713c7983f8f3b235c4d68cc86be2
ChathuraRubasingha <79496529+ChathuraRubasingha@users.noreply.github.com>
2023-10-11 11:11:18 +0530

    side bar bug fix

--- src/Componants/Layout/Sidebar.js
+++ src/Componants/Layout/Sidebar.js
@@ -1,38 +1,47 @@
 import React from "react";
 import "./Layout.css";
-import { FaBuilding, FaClipboardCheck, FaClock, FaCogs, FaShoppingCart, FaUser, FaUsers } from "react-icons/fa";
+import {
+  FaBuilding,
+  FaClipboardCheck,
+  FaClock,
+  FaCogs,
+  FaShoppingCart,
+  FaUser,
+  FaUsers,
+} from "react-icons/fa";
+import { NavLink } from "react-router-dom";
 
 function Sidebar() {
   return (
     <div className="sidebar">
       <div className="navigation">
-        <div className="nav">
+        <NavLink className="nav" activeClassName="active" to="/">
           <FaUser style={{ padding: "0 20px 0 10px" }} />
           My Profile
-        </div>
-        <div className="nav">
+        </NavLink>
+        <NavLink className="nav" activeClassName="active" to="/">
           <FaBuilding style={{ padding: "0 20px 0 10px" }} /> My Clients & Sites
-        </div>
-        <div className="nav">
+        </NavLink>
+        <NavLink className="nav" activeClassName="active" to="/mycleaner">
           <FaUsers style={{ padding: "0 20px 0 10px" }} />
           My Cleaners
-        </div>
-        <div className="nav">
+        </NavLink>
+        <NavLink className="nav" activeClassName="active" to="/">
           <FaShoppingCart style={{ padding: "0 20px 0 10px" }} />
           Orders
-        </div>
-        <div className="nav">
+        </NavLink>
+        <NavLink className="nav" activeClassName="active" to="/">
           <FaCogs style={{ padding: "0 20px 0 10px" }} />
           Operations
-        </div>
-        <div className="nav">
+        </NavLink>
+        <NavLink className="nav" activeClassName="active" to="/">
           <FaClock style={{ padding: "0 20px 0 10px" }} />
           Time on site progress
-        </div>
-        <div className="nav">
+        </NavLink>
+        <NavLink className="nav" activeClassName="active" to="/">
           <FaClipboardCheck style={{ padding: "0 20px 0 10px" }} />
           Attendance Summary
-        </div>
+        </NavLink>
       </div>
       <div className="copyright">Copy right</div>
     </div>
