521b551fc58652b4dff0e54eeb4cea3f4aeb590f
ChathuraRubasingha <79496529+ChathuraRubasingha@users.noreply.github.com>
2023-10-12 23:51:09 +0530

    table

---
+++ src/Componants/Table/Table.js
@@ -0,0 +1,105 @@
+import React from "react";
+
+function Table() {
+  return (
+    <table class="table" style={{margin:'10px 20px'}}>
+      <thead class="thead-light">
+        <tr>
+          <th scope="col">First</th>
+          <th scope="col">Last</th>
+          <th scope="col">Handle</th>
+          <th scope="col">Last</th>
+          <th scope="col">Handle</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <td>Larry</td>
+          <td>the Bird</td>
+          <td>@twitter</td>
+          <td>the Bird</td>
+          <td>@twitter</td>
+        </tr>
+        <tr>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <td>Larry</td>
+          <td>the Bird</td>
+          <td>@twitter</td>
+          <td>the Bird</td>
+          <td>@twitter</td>
+        </tr>
+        <tr>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <td>Larry</td>
+          <td>the Bird</td>
+          <td>@twitter</td>
+          <td>the Bird</td>
+          <td>@twitter</td>
+        </tr>
+        <tr>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <td>Larry</td>
+          <td>the Bird</td>
+          <td>@twitter</td>
+          <td>the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
+
+export default Table;
--- src/Pages/MyCleaner/Mycleaner.js
+++ src/Pages/MyCleaner/Mycleaner.js
@@ -2,16 +2,27 @@ import React from "react";
 import Layout from "../../Componants/Layout/Layout";
 import "./Mycleaner.css";
 import Filter from "../../Componants/Filter/Filter";
+import Table from "../../Componants/Table/Table";
 
 function Mycleaner() {
   return (
     <Layout>
       <div className="cleaner-topic">
         <p>My Cleaners </p>
-        <button className="btn btn-primary">Add</button>
+        <button
+          className="btn btn-primary"
+          style={{
+            padding: "0 20px",
+            color: "white",
+            fontWeight: "500",
+          }}
+        >
+          Add New
+        </button>
       </div>
       <div>
         <Filter />
+        <Table />
       </div>
     </Layout>
   );
