3627778b85dd5fb11413c1dcd1cec8e8b8efab53
ChathuraRubasingha <79496529+ChathuraRubasingha@users.noreply.github.com>
2023-10-13 11:49:16 +0530

    theme

---
+++ src/Theme/palette.js
@@ -0,0 +1,12 @@
+// SETUP COLORS
+const PRIMARY = {
+  black: "#000000", // Pure black
+  darkestGray: "#333333", // Dark gray
+  darkGray: "#666666", // A bit lighter
+  mediumGray: "#999999", // A middle shade of gray
+  lightGray: "#CCCCCC", // Light gray
+  lighterGray: "#F0F0F0", // Even lighter
+  white: "#FFFFFF", // Pure white
+};
+
+export default PRIMARY;
